35bb5399a600f4875d8da32d47d8b74cef4d8519
Breno <nobresilva360@gmail.com>
2025-11-06 21:45:32 -0300

    Transferindo o código da index para form.jx

--- Código/reservar-mesa/src/App.jsx
+++ Código/reservar-mesa/src/App.jsx
@@ -1,1 +1,13 @@
 
+import './App.css'
+import ReservaForm from './component/form'
+
+function App() {
+    return(
+        <div>
+            <ReservaForm />
+        </div>
+    )
+
+}
+export default App
